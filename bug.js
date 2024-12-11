1fe1ccdbde45e8010f028d0cc6d03b562e20331b
BugHunter-X <askzenai@gmail.com>
2024-12-11 04:12:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,14 @@
+```javascript
+// MyComponent.jsx
+
+function MyComponent() {
+  return (
+    <div className="bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {/*This is a simple component that uses Tailwind CSS classes to style the content. */}
+    </div>
+  );
+}
+
+export default MyComponent;
+```
